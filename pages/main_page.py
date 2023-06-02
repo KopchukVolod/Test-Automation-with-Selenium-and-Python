5663314da3762cef3bb1590ffac95341af27757c
Volodymyr Kopchuk <kopchykko@gmail.com>
2023-06-02 19:44:10 +0300

    new class  LoginPageLocators

---
+++ pages/main_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
+
+class MainPage(BasePage):
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
+
+
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+
